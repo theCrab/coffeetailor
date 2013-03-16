d42fa5e6efecef8f0e3f69125c1ed81a96add0ec
The Crab <nelson@21waves.com>
2013-03-16 21:54:50 +0000

    initial ci with paymill

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeetailor::Application.config.secret_token = '029a962c5ba26d99d6266e14216270c26fd2d505e70fe9b0fd6e8ebe11170f79e2f68cfe2fafcc357c2c1d2e1b3fbe71325ef6cba519e23074171bd56a6c04b1'
