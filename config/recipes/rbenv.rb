7f1deeb76e4e41149bd696bc700ab3723e70539b
The Crab <nelson@safarista.com>
2013-04-18 18:13:52 +0100

    changing the rbenv repo

--- config/recipes/rbenv.rb
+++ config/recipes/rbenv.rb
@@ -5,7 +5,7 @@ namespace :rbenv do
   desc "Install rbenv, Ruby, and the Bundler gem"
   task :install, roles: :app do
     run "#{sudo} apt-get -y install curl git-core"
-    run "git clone git://github.com/sstephenson/rbenv.git ~/.rbenv | bash"
+    run "curl https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash"
     bashrc = <<-BASHRC
 if [ -d ${RBENV_ROOT}/.rbenv ]; then
   export PATH="${RBENV_ROOT}/.rbenv/bin:${PATH}"
@@ -15,17 +15,18 @@ BASHRC
     put bashrc, "/tmp/rbenvrc"
     run "cat /tmp/rbenvrc ~/.bashrc > ~/.bashrc.tmp"
     run "mv ~/.bashrc.tmp ~/.bashrc"
-    run %q{export RBENV_ROOT="${HOME}/.rbenv"}
-    run %q{eval "$(rbenv init -)"}
-    # run "rbenv #{rbenv_bootstrap}", :pty => true do |ch, stream, data|
-    #   if data =~ /Press.\[ENTER\].to.continue/
-    #     #prompt, and then send the response to the remote process
-    #     ch.send_data(Capistrano::CLI.password_prompt("Press enter to continue:") + "\n")
-    #   else
-    #     #use the default handler for all other text
-    #     Capistrano::Configuration.default_io_proc.call(ch,stream,data)
-    #   end
-    # end
+    # run %q{export RBENV_ROOT="${HOME}/.rbenv"}
+    # run %q{eval "$(rbenv init -)"}
+    run ". ~/.bashrc"
+    run "rbenv #{rbenv_bootstrap}", :pty => true do |ch, stream, data|
+      if data =~ /Press.\[ENTER\].to.continue/
+        #prompt, and then send the response to the remote process
+        ch.send_data(Capistrano::CLI.password_prompt("Press enter to continue:") + "\n")
+      else
+        #use the default handler for all other text
+        Capistrano::Configuration.default_io_proc.call(ch,stream,data)
+      end
+    end
     run "rbenv install #{ruby_version}"
     run "rbenv global #{ruby_version}"
     run "gem install bundler --no-ri --no-rdoc"
