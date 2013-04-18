d43de18b3ce15ad6c169a793fb19e44df10dfd42
The Crab <nelson@safarista.com>
2013-04-18 15:30:45 +0100

    remove previous cap_bootstrap settings

--- config/recipes/security.rb
+++ config/recipes/security.rb
@@ -1,14 +1,12 @@
-Capistrano::Configuration.instance(:must_exist).load do
-  namespace :security do
-    desc "Setup a firewall with UFW"
-    task :setup_firewall, roles: :web do
-      run "#{sudo} apt-get -y install ufw"
-      run "#{sudo} ufw default deny"
-      run "#{sudo} ufw allow #{port}/tcp"
-      run "#{sudo} ufw allow 80/tcp"
-      run "#{sudo} ufw allow 443/tcp"
-      run "#{sudo} ufw --force enable"
-    end
-    after "deploy:install", "security:setup_firewall"
+namespace :security do
+  desc "Setup a firewall with UFW"
+  task :setup_firewall, roles: :web do
+    run "#{sudo} apt-get -y install ufw"
+    run "#{sudo} ufw default deny"
+    run "#{sudo} ufw allow #{port}/tcp"
+    run "#{sudo} ufw allow 80/tcp"
+    run "#{sudo} ufw allow 443/tcp"
+    run "#{sudo} ufw --force enable"
   end
+  after "deploy:install", "security:setup_firewall"
 end
