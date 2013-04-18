0e5496638f100dc53e816a422e5044faa0fcaf80
The Crab <nelson@safarista.com>
2013-04-18 16:21:51 +0100

    fixed rbenv bootstrap call

--- config/recipes/rbenv.rb
+++ config/recipes/rbenv.rb
@@ -1,5 +1,5 @@
-set_default :ruby_version, "1.9.3-p125"
-set_default :rbenv_bootstrap, "bootstrap-ubuntu-10-04"
+set_default :ruby_version, "1.9.3-p392"
+set_default :rbenv_bootstrap, "bootstrap-ubuntu-12-04"
 
 namespace :rbenv do
   desc "Install rbenv, Ruby, and the Bundler gem"
@@ -7,15 +7,15 @@ namespace :rbenv do
     run "#{sudo} apt-get -y install curl git-core"
     run "curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash"
     bashrc = <<-BASHRC
-if [ -d $HOME/.rbenv ]; then
-  export PATH="$HOME/.rbenv/bin:$PATH"
+if [ -d ${RBENV_ROOT}/.rbenv ]; then
+  export PATH="${RBENV_ROOT}/.rbenv/bin:${PATH}"
   eval "$(rbenv init -)"
 fi
 BASHRC
     put bashrc, "/tmp/rbenvrc"
     run "cat /tmp/rbenvrc ~/.bashrc > ~/.bashrc.tmp"
     run "mv ~/.bashrc.tmp ~/.bashrc"
-    run %q{export PATH="$HOME/.rbenv/bin:$PATH"}
+    run %q{export RBENV_ROOT="${HOME}/.rbenv"}
     run %q{eval "$(rbenv init -)"}
     run "rbenv #{rbenv_bootstrap}"
     run "rbenv install #{ruby_version}"
