fa9a1dfb30daa0da0d9d651a6e96ae7bfca3053d
The Crab <nelson@safarista.com>
2013-04-19 12:09:55 +0100

    fix typo for .gemrc

--- config/recipes/rbenv.rb
+++ config/recipes/rbenv.rb
@@ -4,12 +4,9 @@ set_default :rbenv_bootstrap, "bootstrap-ubuntu-12-04"
 namespace :rbenv do
   desc "Install rbenv, Ruby, and the Bundler gem"
   task :install, roles: :app do
-    run "#{sudo} apt-get -y install curl git-core"
-    run "#{sudo} apt-get -y install build-essential ruby-dev openssl libopenssl-ruby1.9.1 libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison"
-    # run "#{sudo} rm -rf ~/.rbenv/plugins/ruby-build"
-    # run "#{sudo} mkdir -p ~/.rbenv/plugins"
-    run "curl https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash"
-    # run "git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build"
+    # run "#{sudo} apt-get -y install curl git-core"
+    # run "#{sudo} apt-get -y install build-essential ruby-dev openssl libopenssl-ruby1.9.1 libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison"
+    # run "curl https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash"
 #     bashrc = <<-BASHRC
 # if [ -d ${RBENV_ROOT}/.rbenv ]; then
 #   export PATH="${RBENV_ROOT}/.rbenv/bin:${PATH}"
@@ -32,11 +29,11 @@ namespace :rbenv do
     #   end
     # end
 
-    run "rbenv install #{ruby_version}"
-    run "rbenv rehash"
-    run "rbenv global #{ruby_version}"
-    run "#{sudo} echo 'install: --no-ri --no-rdoc' >> ~./gemrc"
-    run "#{sudo} echo 'update: --no-ri --no-rdoc' >> ~./gemrc"
+    # run "rbenv install #{ruby_version}"
+    # run "rbenv rehash"
+    # run "rbenv global #{ruby_version}"
+    run "#{sudo} echo 'install: --no-ri --no-rdoc' >> ~/.gemrc"
+    run "#{sudo} echo 'update: --no-ri --no-rdoc' >> ~/.gemrc"
     run "gem install bundler"
     run "rbenv rehash"
   end
