7ac76c029ca112a940816031106475124afe17f0
The Crab <nelson@safarista.com>
2013-04-19 11:41:53 +0100

    add more libs and lib-yaml for the install to continue easily

--- config/recipes/rbenv.rb
+++ config/recipes/rbenv.rb
@@ -5,7 +5,10 @@ namespace :rbenv do
   desc "Install rbenv, Ruby, and the Bundler gem"
   task :install, roles: :app do
     run "#{sudo} apt-get -y install curl git-core"
+    run "#{sudo} apt-get -y install build-essential ruby-dev openssl libopenssl-ruby1.9.1 libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison"
+    run "#{sudo} mkdir -p ~/.rbenv/plugins"
     run "curl https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash"
+    run "git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build"
 #     bashrc = <<-BASHRC
 # if [ -d ${RBENV_ROOT}/.rbenv ]; then
 #   export PATH="${RBENV_ROOT}/.rbenv/bin:${PATH}"
@@ -16,8 +19,8 @@ namespace :rbenv do
 #     run "cat /tmp/rbenvrc ~/.bashrc > ~/.bashrc.tmp"
 #     run "mv ~/.bashrc.tmp ~/.bashrc"
 #     run %q{export RBENV_ROOT="${HOME}/.rbenv/bin:${PATH}"}
-#     # run %q{eval "$(rbenv init -)"}
-#     run "source ~/.bashrc"
+#     run %q{eval "$(rbenv init -)"}
+#     run "exec $SHELL"
     run "rbenv #{rbenv_bootstrap}", :pty => true do |ch, stream, data|
       if data =~ /Press.\[ENTER\].to.continue/
         #prompt, and then send the response to the remote process
@@ -27,11 +30,13 @@ namespace :rbenv do
         Capistrano::Configuration.default_io_proc.call(ch,stream,data)
       end
     end
-    run "rbenv rehash"
-    run "rbenv update"
+
     run "rbenv install #{ruby_version}"
+    run "rbenv rehash"
     run "rbenv global #{ruby_version}"
-    run "gem install bundler --no-ri --no-rdoc"
+    run "#{sudo} echo 'install: --no-ri --no-rdoc' >> ~./gemrc"
+    run "#{sudo} echo 'update: --no-ri --no-rdoc' >> ~./gemrc"
+    run "gem install bundler"
     run "rbenv rehash"
   end
   after "deploy:install", "rbenv:install"
