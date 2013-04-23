18e1374e2b707d02d54fe62eb8f801aac324596f
The Crab <nelson@safarista.com>
2013-04-23 01:05:30 +0100

    fixing the db:seed path

--- config/recipes/seed.rb
+++ config/recipes/seed.rb
@@ -1,6 +1,6 @@
 namespace :deploy do
   desc "Create needed records in the database with its default values"
   task :seed do
-    run "cd {current_path}; bundle exec rake db:seed RAILS_ENV=production"
+    run "bundle exec rake db:seed RAILS_ENV=production"
   end
 end
