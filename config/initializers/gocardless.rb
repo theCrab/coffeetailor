e7ade655b2c4c6ff3bf65b7a54c3a25ae79258c7
The Crab <nelson@safarista.com>
2013-05-02 13:37:16 +0100

    Add initializer for gocardless

---
+++ config/initializers/gocardless.rb
@@ -0,0 +1,15 @@
+gc_payment_method = Spree::PaymentMethod.find_by_type("Spree::PaymentMethod::Gocardless")
+
+if !Rails.env.production? && gc_payment_method.preferred_test_mode && gc_payment_method.preferred_server == 'test'
+  puts "***********===> Gocardless <=== is in sandbox mode***********"
+  GoCardless.environment = :sandbox
+end
+
+unless gc_payment_method.blank?
+  GoCardless.account_details = {
+    :app_id      => gc_payment_method.preferred_app_id.to_s,
+    :app_secret  => gc_payment_method.preferred_app_secret.to_s,
+    :token       => gc_payment_method.preferred_token.to_s,
+    :merchant_id => gc_payment_method.preferred_merchant_id.to_s
+  }
+end
